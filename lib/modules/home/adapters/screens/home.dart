266ffc7c42590bb858bcecf8be36f65b95fd0cbc
ThayliAreliVillegas112 <83984948+ThayliAreliVillegas112@users.noreply.github.com>
2023-12-01 13:47:31 -0600

    Update home.dart

--- lib/modules/home/adapters/screens/home.dart
+++ lib/modules/home/adapters/screens/home.dart
@@ -1,37 +1,37 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( backgroundColor: Colors.white,foregroundColor: Colors.black54,
-      actions: <Widget>[
-            const SizedBox(width: 15),
-            IconButton(
-              icon: const Icon(Icons.format_align_justify, color: Colors.black),
-              onPressed: () {},
-            ),
-
-            const Spacer(),
-            IconButton(
-              icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-              onPressed: () {},
-            ),
-            const Spacer(),
-            IconButton(
-              icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-              onPressed: () {},
-            ),
-            const SizedBox(width: 15),
-          ],
-        ),
-        body:  SingleChildScrollView(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black54,
+        actions: <Widget>[
+          const SizedBox(width: 15),
+          IconButton(
+            icon: const Icon(Icons.format_align_justify, color: Colors.black),
+            onPressed: () {},
+          ),
+          const Spacer(),
+          IconButton(
+            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
+            onPressed: () {},
+          ),
+          const Spacer(),
+          IconButton(
+            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
+            onPressed: () {},
+          ),
+          const SizedBox(width: 15),
+        ],
+      ),
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children:[
+            padding: EdgeInsets.all(16.0),
+            child: Column(children: [
               Row(
                 children: [
                   const CircleAvatar(
@@ -48,36 +48,40 @@ class Home extends StatelessWidget{
                         height: 150.0,
                         color: Colors.grey,
                       ),
-                      Row(
-                        children:[
-                          
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(width: 30,),
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(width: 30,),
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(width: 30,),
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                        ]
-                      ),
-                      
+                      Row(children: [
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                      ]),
                     ],
                   ),
-                  
                 ],
-            
               ),
               const Divider(
                 color: Colors.grey,
@@ -99,36 +103,40 @@ class Home extends StatelessWidget{
                         height: 150.0,
                         color: Colors.grey,
                       ),
-                      Row(
-                        children:[
-                          
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(width: 30,),
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(width: 30,),
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(width: 30,),
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                        ]
-                      ),
-                      
+                      Row(children: [
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                      ]),
                     ],
                   ),
-                  
                 ],
-            
               ),
               //divider
               const Divider(
@@ -151,36 +159,40 @@ class Home extends StatelessWidget{
                         height: 150.0,
                         color: Colors.grey,
                       ),
-                      Row(
-                        children:[
-                          
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(width: 30,),
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(width: 30,),
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(width: 30,),
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                        ]
-                      ),
-                      
+                      Row(children: [
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                      ]),
                     ],
                   ),
-                  
                 ],
-            
               ),
               //divider
               const Divider(
@@ -203,43 +215,51 @@ class Home extends StatelessWidget{
                         height: 150.0,
                         color: Colors.grey,
                       ),
-                      Row(
-                        children:[
-                          
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(width: 30,),
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(width: 30,),
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                          const SizedBox(width: 30,),
-                          IconButton(
-                            icon: const Icon(Icons.star_border_outlined, color: Colors.black),
-                            onPressed: () {},
-                          ),
-                        ]
-                      ),
-                      
+                      Row(children: [
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star_border_outlined,
+                              color: Colors.black),
+                          onPressed: () {},
+                        ),
+                      ]),
                     ],
                   ),
-                  
                 ],
-            
               ),
-            ]
-          )
-          
-        ),
-      )
+            ])),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Aquí puedes agregar la lógica para abrir la pantalla de creación de un nuevo post.
+          print('Botón flotante presionado');
+        },
+        child: Icon(Icons.edit),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
-
 }
