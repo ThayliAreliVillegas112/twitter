24d4093cd2bc88286f252494575fcb3b7fafad90
ThayliAreliVillegas112 <83984948+ThayliAreliVillegas112@users.noreply.github.com>
2023-12-01 14:08:11 -0600

    Update profile.dart

--- lib/modules/profile/adapters/screens/profile.dart
+++ lib/modules/profile/adapters/screens/profile.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget{
+class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text('HOLA DESDE Profile.DART')
-        ]
-      )
-    );
+    return Scaffold(
+        body: Column(children: [
+      Container(
+        width: 400, // Ajusta según sea necesario
+        height: 180.0,
+        color: Colors.grey,
+        padding: EdgeInsets.only(left: 16.0, top: 80),
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 50.0, // Ajusta según sea necesario
+              backgroundColor: Colors.blue, // Ajusta según sea necesario
+              // Agrega cualquier configuración adicional que necesites
+            ),
+          ],
+        ),
+      ),
+    ]));
   }
-
 }
